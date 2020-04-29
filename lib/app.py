9be9c014e0297db6f94343c0444d95b15e53fa69
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-04-29 02:12:08 -0400

    finished homework

--- lib/app.py
+++ lib/app.py
@@ -2,8 +2,35 @@ import random
 
 
 def RandomNumber():
+    # - Pick a random number between 0 and 100
     random_number = random.randrange(0, 100)
-    # test for random #:
+
+    # - Test for random #:
     # print(random_number)
-    print("What do you think the random number is?")
-    user_guess = int(input())
+
+    # - Ask the user to guess the number
+    print("Guess our random # between 0-100:")
+    guess = int(input())
+    print(f"Your guess is {guess}")
+
+    # If the guess is correct, tell the user they guessed correctly and ask if
+    # they'd like to play again
+    while guess != random_number:
+        print(f"{guess} is not correct! It was {random_number}! :(")
+        print("Would you like to play again? (Yes or No)")
+        play_again = str(input())
+        if play_again == "Yes":
+            RandomNumber()
+        else:
+            return print("Goodbye!")
+    while guess == random_number:
+        print(f"{guess} is correct! :) ")
+        print("Would you like to play again? (Yes or No)")
+        play_again = str(input())
+        if play_again == "Yes":
+            RandomNumber()
+        else:
+            return print("Goodbye!")
+
+
+RandomNumber()
