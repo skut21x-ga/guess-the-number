9b7037b8e50285fec889e97405c7451a619b43cc
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-04-29 17:55:48 -0400

    fixed code. stylized.

--- lib/app.py
+++ lib/app.py
@@ -6,31 +6,45 @@ def RandomNumber():
     random_number = random.randrange(0, 100)
 
     # - Test for random #:
-    # print(random_number)
+    print(random_number)
 
     # - Ask the user to guess the number
-    print("Guess our random # between 0-100:")
-    guess = int(input())
-    print(f"Your guess is {guess}")
+    print("")
+    print(" ------------------------------------------- ")
+    print("     ❓ Guess a random # between 0-100: ❓")
+    print(" ------------------------------------------- ")
+    print("")
 
+    guess = int(input())
     # If the guess is correct, tell the user they guessed correctly and ask if
     # they'd like to play again
     while guess != random_number:
-        print(f"{guess} is not correct! It was {random_number}! :(")
-        print("Would you like to play again? (Yes or No)")
-        play_again = str(input())
-        if play_again == "Yes":
-            RandomNumber()
-        else:
-            return print("Goodbye!")
+        print("")
+        print(" ------------------------------------------ ")
+        print(f"    🚨 Your guess '{guess}' is not correct! 🚨")
+        print("             Please guess again:")
+        print(" ------------------------------------------ ")
+        print("")
+
+        guess = int(input())
     while guess == random_number:
-        print(f"{guess} is correct! :) ")
-        print("Would you like to play again? (Yes or No)")
+        print("")
+        print(" ------------------------------------------ ")
+        print(f"          🎉  {guess} is correct!  🎉")
+        print("       Would you like to play again?")
+        print("               (Yes or No)")
+        print(" ------------------------------------------")
+        print("")
         play_again = str(input())
         if play_again == "Yes":
             RandomNumber()
         else:
-            return print("Goodbye!")
+            print("")
+            print(" ------------------------------------------ ")
+            print("         😕 Oh... Okay. Goodbye! 😕")
+            print(" ------------------------------------------ ")
+            print("")
+            exit()
 
 
 RandomNumber()
